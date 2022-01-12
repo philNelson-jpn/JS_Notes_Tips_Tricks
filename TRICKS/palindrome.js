76d9b6356e24b11b947fff8fd2e702f06dd311de
Philip Nelson <nelsonpa@miamioh.edu>
2022-01-12 11:35:13 +0900

    updated palindrome.js with a new palindrome

--- TRICKS/palindrome.js
+++ TRICKS/palindrome.js
@@ -5,14 +5,14 @@
 
 function palindrome(string){
     // First, lowercase the string and use RegExp to remove unwanted characters
-    const re = /[\W_]/g;
-    const lowRegStr = string.toLowerCase().replace(re, '');
+    const re = /[\W_]/g
+    const lowRegStr = string.toLowerCase().replace(re, '')
 
     // Reverse the string and save it in a new variable
-    const reverseStr = lowRegStr.split('').reverse().join('');
+    const reverseStr = lowRegStr.split('').reverse().join('')
 
     // Check if reverseStr is strictly equals to lowRegStr
-    console.log(reverseStr === lowRegStr);
+    console.log(reverseStr === lowRegStr)
 }
 
-palindrome("race car")
+palindrome("Yo, banana boy!")
