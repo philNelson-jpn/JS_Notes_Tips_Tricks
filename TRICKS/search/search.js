4ae804ac1142c40086f7573c1a0077199f3763d8
Philip Nelson <nelsonpa@miamioh.edu>
2022-02-01 13:55:37 +0900

    Search that automagically returns results based on the value of the search input (any part of username or email). Content fetched from json placeholder api

---
+++ TRICKS/search/search.js
@@ -0,0 +1,42 @@
+// grab the template that will be used to populate the search results
+const userCardTemplate = document.querySelector("[data-user-template]")
+// The whole container where we will append data
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+// search input that we can add an event listener on
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    // set the card as visible if the input matches any part of the username or email
+    users.forEach(user => {
+        const isVisible = 
+        user.name.toLowerCase().includes(value) || 
+        user.email.toLowerCase().includes(value)
+        // if the element does not include any of the input, toggle the hide class
+        user.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+// grab an api of fake users
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+    users = data.map(user => {
+        // get the content inside our template, clone it and return a document fragment
+        const card = userCardTemplate.content.cloneNode(true).children[0]
+        // get the first child of the card (with all of the content inside it (body,header))
+        const header = card.querySelector("[data-header]")
+        const body = card.querySelector("[data-body]")
+
+        header.textContent = user.name
+        body.textContent = user.email
+
+        userCardContainer.append(card)
+
+        // Does the name/email include
+        return { name: user.name, email: user.email, element: card}
+    })
+
+})
