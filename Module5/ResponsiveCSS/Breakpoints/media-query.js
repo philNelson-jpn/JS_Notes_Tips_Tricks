83f114ef6831c93b679021c3e664ded9b215ecac
Phil Nelson <nelsonpa@miamioh.edu>
2022-03-10 15:48:27 +0900

    added constants.js file that can be used to calculate media queries for different client-side devices with rems instead of pixels. This file is meant to be used with styled-components in React

---
+++ Module5/ResponsiveCSS/Breakpoints/media-query.js
@@ -0,0 +1,13 @@
+// Template for media queries to be used in styled-components
+
+// constants.js
+const BREAKPOINTS = {
+    tabletMin: 550,
+    laptopMin: 1100,
+    desktopMin: 1500,
+  }
+  const QUERIES = {
+    'tabletAndUp': `(min-width: ${BREAKPOINTS.tabletMin / 16}rem)`,
+    'laptopAndUp': `(min-width: ${BREAKPOINTS.laptopMin / 16}rem)`,
+    'desktopAndUp': `(min-width: ${BREAKPOINTS.desktopMin / 16}rem)`,
+  }
