1450bb2cd1b11db84e793b92278d3fd0140fc9cc
Phil Nelson <nelsonpa@miamioh.edu>
2022-03-11 08:45:47 +0900

    recreated media queries constants to be used with styled-components in react and included an example of its use.

--- Module5/ResponsiveCSS/Breakpoints/media-query.js
+++ Module5/ResponsiveCSS/Breakpoints/media-query.js
@@ -1,13 +1,39 @@
 // Template for media queries to be used in styled-components
 
 // constants.js
-const BREAKPOINTS = {
-    tabletMin: 550,
+// const BREAKPOINTS = {
+//     tabletMin: 550,
+//     laptopMin: 1100,
+//     desktopMin: 1500,
+//   }
+//   const QUERIES = {
+//     'tabletAndUp': `(min-width: ${BREAKPOINTS.tabletMin / 16}rem)`,
+//     'laptopAndUp': `(min-width: ${BREAKPOINTS.laptopMin / 16}rem)`,
+//     'desktopAndUp': `(min-width: ${BREAKPOINTS.desktopMin / 16}rem)`,
+//   }
+
+
+
+  const BREAKPOINTS = {
+    tabletMin: 500,
     laptopMin: 1100,
-    desktopMin: 1500,
+    desktopMin: 1500
   }
+
   const QUERIES = {
-    'tabletAndUp': `(min-width: ${BREAKPOINTS.tabletMin / 16}rem)`,
+    'tabletAndUp':`(min-width: ${BREAKPOINTS.tabletMin / 16}rem)`,
     'laptopAndUp': `(min-width: ${BREAKPOINTS.laptopMin / 16}rem)`,
     'desktopAndUp': `(min-width: ${BREAKPOINTS.desktopMin / 16}rem)`,
   }
+
+//   Example of how it can be used.
+
+//   import { QUERIES } from '../../constants';
+
+  const WRAPPER = styled.div`
+    padding: 10px;
+
+    @media (${QUERIES.tabletAndUp}){
+        padding: 20px;
+    }
+  `;
