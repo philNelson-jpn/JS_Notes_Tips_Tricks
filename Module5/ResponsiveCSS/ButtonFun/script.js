45e65412d2b9816c8e1bb6c78b3324c3ef0c3847
Phil Nelson <nelsonpa@miamioh.edu>
2022-03-11 09:14:40 +0900

    added another setProperty to increase the padding incrementally

--- Module5/ResponsiveCSS/ButtonFun/script.js
+++ Module5/ResponsiveCSS/ButtonFun/script.js
@@ -1,10 +1,16 @@
 let fontSize = 1;
+let padding = 1;
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
     fontSize += 0.25;
+    padding += 2;
     button.style.setProperty(
         '--inflated-size',
         fontSize + 'rem'
+    );
+    button.style.setProperty(
+        '--inflated-padding',
+        padding + 'rem'
     );
 });
