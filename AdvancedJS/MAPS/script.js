35e20d3ab3b2a81dd0e0b7d597700d84dcf8f9ad
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-09 16:09:03 +0900

    intro to maps in JS

---
+++ AdvancedJS/MAPS/script.js
@@ -0,0 +1,22 @@
+/*
+const CURRENCY_MAP = {
+    "United States": "USD",
+    India: "Rupee",
+}
+
+const currency = CURRENCY_MAP["India"]
+
+console.log(currency)
+*/
+// OR
+/*
+const CURRENCIES = [
+    { name: "USD", country: "United States" },
+    { name: "Rupee", country: "India"}
+]
+
+const currency = CURRENCIES.find(currency => currency.country === "India").name
+
+console.log(currency)
+*/
+
