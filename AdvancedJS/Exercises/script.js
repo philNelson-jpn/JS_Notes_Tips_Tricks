15618400a60deece4aa973f3d15afed7c58a8677
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-12 09:29:59 +0900

    remove duplicates from an array and create a function that grabs an id from a map and returns the values

--- AdvancedJS/Exercises/script.js
+++ AdvancedJS/Exercises/script.js
@@ -8,11 +8,39 @@ const person = {
     }
 }
 
-function greet(firstName = "human.", salutation = "Hello there,"){
-    console.log(`${salutation} ${firstName}`)
+const array = [1, 2, 3, 4, 4, 3, 2, 5]
+const array2 = ["A", "B", "A", "W", "C", "D", "B"]
+
+function removeDuplicates(array){
+    return [...new Set(array)]
 }
 
-greet()
+console.log(removeDuplicates(array))
+// (5) [1, 2, 3, 4, 5]
+console.log(removeDuplicates(array2))
+
+
+/*
+Map Exercise
+const idMap = new Map([
+    [1, {test: "test1", description: "desc1"}],
+    [2, {test: "test2", description: "desc2"}],
+    [3, {test: "test3", description: "desc3"}],
+])
+
+function getId(id){
+    return idMap.get(id)
+}
+
+console.log(getId(2))
+// {test: "test2", description: "desc2"}
+
+
+// function greet(firstName = "human.", salutation = "Hello there,"){
+//     console.log(`${salutation} ${firstName}`)
+// }
+
+// greet()
 
 // const {name: firstName} = person
 // console.log(firstName)
