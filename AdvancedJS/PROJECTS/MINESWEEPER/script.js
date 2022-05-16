6ef3151b6fc982132afbe20d45ba831c3b7ca6d5
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-16 15:40:08 +0900

    display full minesweeper board on the screen, no functionality yet.

---
+++ AdvancedJS/PROJECTS/MINESWEEPER/script.js
@@ -0,0 +1,22 @@
+// Display/UI
+
+import { createBoard } from "./minesweeper.js";
+
+const BOARD_SIZE = 5
+const NUMBER_OF_MINES = 2
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+const boardElement = document.querySelector(".board")
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+    })
+})
+boardElement.style.setProperty('--size', BOARD_SIZE)
+
+
+// Populate the board with tiles/mines
+// Left click on tiles - reveal
+// Right click on tiles - mark
+// Check for win/lose
