255bb33cd1ef22e487008a0f7ff34c5382bf7c7a
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-18 11:12:27 +0900

    added marking tiles functionality and decrementing mine count

--- AdvancedJS/PROJECTS/MINESWEEPER/minesweeper.js
+++ AdvancedJS/PROJECTS/MINESWEEPER/minesweeper.js
@@ -1,6 +1,6 @@
 // Logic
 
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
     HIDDEN: "hidden",
     MINE: "mine",
     NUMBER: "number",
@@ -22,7 +22,7 @@ export function createBoard(boardSize, numberOfMines){
                 element,
                 x,
                 y,
-                mine: true,
+                mine: minePositions.some(positionMatch.bind(null, { x, y })),
                 get status(){
                     return element.dataset.status
                 },
@@ -36,6 +36,21 @@ export function createBoard(boardSize, numberOfMines){
     board.push(row)
     }
     return board
+}
+
+export function markTile(tile){
+    if (
+        tile.status !== TILE_STATUSES.HIDDEN && 
+        tile.status !== TILE_STATUSES.MARKED
+    ) {
+        return
+    }
+
+    if (tile.status === TILE_STATUSES.MARKED){
+        tile.status = TILE_STATUSES.HIDDEN
+    } else {
+        tile.status = TILE_STATUSES.MARKED
+    }
 }
 
 function getMinePositions(boardSize, numberOfMines){
