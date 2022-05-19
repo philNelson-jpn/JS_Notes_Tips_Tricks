a45639a462631cbde2399174fe1aa63128281b01
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-19 14:18:10 +0900

    completed math solver exercise with any amount of multiplication, division, addition, and subtraction checked with regex and switch statements

--- AdvancedJS/PROJECTS/MATHSOLVER/script.js
+++ AdvancedJS/PROJECTS/MATHSOLVER/script.js
@@ -2,6 +2,12 @@ const inputElement = document.getElementById("equation")
 const outputElement = document.getElementById("results")
 const form = document.getElementById("equation-form")
 
+// Get a digit that is surrounded by any amount of white space on both sides that has a / or * inbetween (checks for division or multiplication) \S takes into account negative numbers and very large numbers (just \d only takes in digits)
+const MULTIPLY_DIVIDE_REGEX = /(?<operand1>\S+)\s*(?<operation>[\/\*])\s*(?<operand2>\S+)/
+// Repeat for addition and subtraction
+// include the lookback (?<!e) to check for scientific notation output that returns - and ignores it
+const ADD_SUBTRACT_REGEX = /(?<operand1>\S+)\s*(?<operation>(?<!e)[\-\+])\s*(?<operand2>\S+)/
+
 form.addEventListener("submit", e => {
     e.preventDefault()
 
@@ -10,5 +16,34 @@ form.addEventListener("submit", e => {
 })
 
 function parse(equation){
-    return equation
+    if (equation.match(MULTIPLY_DIVIDE_REGEX)){
+        const result = handleMath(equation.match(MULTIPLY_DIVIDE_REGEX).groups)
+        // replace the result of division/multiplication as the new number to be added/subtracted from any remaining numbers in the equation (2 + 2 * 6 will be replaced with 2 + 12)
+        const newEquation = equation.replace(MULTIPLY_DIVIDE_REGEX, result)
+        return parse(newEquation)
+    }   else  if (equation.match(ADD_SUBTRACT_REGEX)){
+        const result = handleMath(equation.match(ADD_SUBTRACT_REGEX).groups)
+        const newEquation = equation.replace(ADD_SUBTRACT_REGEX, result)
+        return parse(newEquation)
+    }
+    else {
+        return parseFloat(equation)
+        // return NaN if letters are used in input
+    }
+}
+
+function handleMath( { operand1, operand2, operation } ){
+    const number1 = parseFloat(operand1)
+    const number2 = parseFloat(operand2)
+
+    switch(operation){
+        case '*':
+            return number1 * number2
+        case '/':
+            return number1 / number2 
+        case '+':
+            return number1 + number2
+        case '-':
+            return number1 - number2                       
+    }
 }
