06a327af9ac4b06c69875801ae02b705fad81f52
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-19 13:39:12 +0900

    initial commit for math solver exercise that displays the value of the input under the input

---
+++ AdvancedJS/PROJECTS/MATHSOLVER/script.js
@@ -0,0 +1,14 @@
+const inputElement = document.getElementById("equation")
+const outputElement = document.getElementById("results")
+const form = document.getElementById("equation-form")
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const result = parse(inputElement.value)
+    outputElement.textContent = result
+})
+
+function parse(equation){
+    return equation
+}
