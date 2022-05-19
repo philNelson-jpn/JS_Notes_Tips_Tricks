5c0136c689fc7cf6893ae80abd2eb80366956bdc
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-19 15:48:57 +0900

    added regex for exponents and parenthesis

--- AdvancedJS/PROJECTS/MATHSOLVER/script.js
+++ AdvancedJS/PROJECTS/MATHSOLVER/script.js
@@ -7,6 +7,9 @@ const MULTIPLY_DIVIDE_REGEX = /(?<operand1>\S+)\s*(?<operation>[\/\*])\s*(?<oper
 // Repeat for addition and subtraction
 // include the lookback (?<!e) to check for scientific notation output that returns - and ignores it
 const ADD_SUBTRACT_REGEX = /(?<operand1>\S+)\s*(?<operation>(?<!e)[\-\+])\s*(?<operand2>\S+)/
+// Repeat for parenthesis && exponent
+const PARENTHESIS_REGEX = /\((?<equation>[^\(\)]*)\)/
+const EXPONENT_REGEX = /(?<operand1>\S+)\s*(?<operation>\^)\s*(?<operand2>\S+)/
 
 form.addEventListener("submit", e => {
     e.preventDefault()
@@ -16,7 +19,16 @@ form.addEventListener("submit", e => {
 })
 
 function parse(equation){
-    if (equation.match(MULTIPLY_DIVIDE_REGEX)){
+    if (equation.match(PARENTHESIS_REGEX)) {
+        const subEquation = equation.match(PARENTHESIS_REGEX).groups.equation
+        const result = parse(subEquation)
+        const newEquation = equation.replace(PARENTHESIS_REGEX, result)
+        return parse(newEquation)
+      } else if (equation.match(EXPONENT_REGEX)) {
+        const result = handleMath(equation.match(EXPONENT_REGEX).groups)
+        const newEquation = equation.replace(EXPONENT_REGEX, result)
+        return parse(newEquation)
+      } else if (equation.match(MULTIPLY_DIVIDE_REGEX)){
         const result = handleMath(equation.match(MULTIPLY_DIVIDE_REGEX).groups)
         // replace the result of division/multiplication as the new number to be added/subtracted from any remaining numbers in the equation (2 + 2 * 6 will be replaced with 2 + 12)
         const newEquation = equation.replace(MULTIPLY_DIVIDE_REGEX, result)
@@ -44,6 +56,8 @@ function handleMath( { operand1, operand2, operation } ){
         case '+':
             return number1 + number2
         case '-':
-            return number1 - number2                       
+            return number1 - number2     
+        case "^":
+            return number1 ** number2                              
     }
 }
