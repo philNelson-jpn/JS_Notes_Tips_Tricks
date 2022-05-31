564715da17a303f73ae6d234495b63dd7ddc18e6
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-31 10:39:11 +0900

    Converting normal functions to arrow functions

---
+++ BeginnerJS/ArrowFunctions/script.js
@@ -0,0 +1,45 @@
+// ARROW FUNCTIONS
+/*
+function printName(name){
+    console.log(`It's ya boy, ${name}`)
+}
+
+const printArrow = (name) => {
+    console.log(`It's ya boy, ${name}`)
+}
+
+printArrow("Phil")
+// It's ya boy, Phil
+*/
+/*
+function printHi(name){
+    return `Hi, ${name}`
+}
+
+const printArrowHi = name => `Hi, ${name}`
+
+console.log(printArrowHi("Phil"))
+// Hi, Phil
+*/
+
+/*
+function hi(){
+    console.log("Hi")
+}
+
+const hiArrow = () => console.log("Hi")
+
+hiArrow()
+// Hi
+*/
+
+// Anonymous Arrow Functions
+
+function func(x, callback){
+    callback(x)
+}
+
+func(10, variable => console.log(variable))
+
+
+
