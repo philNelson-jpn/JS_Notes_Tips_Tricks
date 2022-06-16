595a5e8a47152137a88d48e0e60df3e6d3a398ec
Phil Nelson <nelsonpa@miamioh.edu>
2022-06-16 13:16:11 +0900

    Update an object full of albums with unique ids to include tracks or artist titles if they are not included, while loop review

--- BeginnerJS/EXERCISES/script.js
+++ BeginnerJS/EXERCISES/script.js
@@ -541,3 +541,71 @@ console.log(returnProp(lookup, "echo"))
 console.log(returnProp(lookup, "beebee"))
 // Not Found
 */
+// Update Album/Music collection
+/*
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  }
+
+  function updateRecords(records, id, prop, value) {
+// If "prop" isn't "tracks" and "value" isn't an empty string, update or set that album's "prop" to "value".
+    if(prop !== "tracks" && value !== ""){
+        records[id][prop] = value
+        // If "prop" is "tracks" but the album doesn't have a "tracks" property, create an empty array and add "value" to it.
+    }else if(prop === "tracks" && records[id].hasOwnProperty("tracks") === false){
+        records[id][prop] = [value]
+        // If "prop" is "tracks" and "value" isn't an empty string, add "value" to the end of the album's existing "tracks" array.
+    } else if(prop === "tracks" && value !== ""){
+        records[id][prop].push(value)
+        // If value is an empty string, delete the given prop property from the album.
+    } else if(value === ""){
+        delete records[id][prop]
+    }
+    return records
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+  console.log(recordCollection[5439])
+
+  /*
+    if(value !== "" && prop !== "tracks"){
+      records[id][prop] = value
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false){
+      records[id][prop] = [value]
+    } else if (prop === "tracks" && value !== ""){
+      records[id][prop].push(value)
+    } else if (value === ""){
+      delete records[id][prop]
+    }
+  */
+
+const myArray = [];
+let i = 5
+
+while(i >= 0){
+    myArray.push(i)
+    i--
+}
+
+console.log(myArray)
+// [5, 4, 3, 2, 1, 0]
+
+
+
+    
