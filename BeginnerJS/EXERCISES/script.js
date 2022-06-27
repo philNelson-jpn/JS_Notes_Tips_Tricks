1366550b2124b2594954e795bb73b8bf25460cee
Phil Nelson <nelsonpa@miamioh.edu>
2022-06-27 09:08:45 +0900

    loop through a list of contacts and return properties with a function that takes in the contact's name and a prop, anonymous arrow functions

--- BeginnerJS/EXERCISES/script.js
+++ BeginnerJS/EXERCISES/script.js
@@ -1128,3 +1128,58 @@ function printNames(currentPerson){
 
 printNames(person)
 */
+// 6-27-2022
+/*
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ]
+
+  function lookUpProfile(name, prop){
+    for(let i = 0; i < contacts.length; i++){
+        if(contacts[i].firstName === name){
+            if(contacts[i].hasOwnProperty(prop)){
+                return contacts[i][prop]
+            }else{
+                return "No such property"
+            }
+        }
+    }
+    return "Profile does not exist"
+
+  }
+
+  console.log(lookUpProfile("Akira", "age"))
+  */
+
+//   Create an anonymous ARROW function as the callback that prints out 10.
+/*
+function func(x, callback){
+    callback(x)
+}
+
+func(10, (value) => console.log(value))
+*/
+
--- Regex/script.js
+++ Regex/script.js
@@ -1,4 +1,7 @@
 let twinkleStar = "Twinkle, twinkle, little star"
 let starRegex = /twinkle/gi
 let result = twinkleStar.match(starRegex)
+
 console.log(result)
+// ['Twinkle', 'twinkle']
+
