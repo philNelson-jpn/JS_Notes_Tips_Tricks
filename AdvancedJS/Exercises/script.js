991c48c0192d10e27f77457af02318b577f2de9d
Phil Nelson <nelsonpa@miamioh.edu>
2022-10-11 10:32:09 +0900

    handling input and submit states in React, simulating api calls in lifestyle method componentDidMount with setTimeout

--- AdvancedJS/Exercises/script.js
+++ AdvancedJS/Exercises/script.js
@@ -5904,4 +5904,64 @@ function getId(id){
 
 console.log(getId(2))
 */
+/*
+class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        submit: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      });
+    }
+    handleSubmit(event) {
+      event.preventDefault()
+      this.setState({
+        submit: this.state.input
+      })
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.input} onChange={this.handleChange}/>
+            <button type='submit'>Submit!</button>
+          </form>
+            <h1>{this.state.submit}</h1>
+        </div>
+      );
+    }
+  }
+*/
+/*
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeUsers: null
+      };
+    }
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          activeUsers: 1273
+        });
+      }, 500);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Active Users: {this.state.activeUsers}</h1>
+        </div>
+      );
+    }
+  }
+  */
 
+  
