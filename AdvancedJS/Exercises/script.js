8ec0bf42b6fb7149aae320c0154efdd59aa05454
Phil Nelson <nelsonpa@miamioh.edu>
2022-11-10 09:39:31 +0900

    truth test function that takes an array and a function that loops over the array

--- AdvancedJS/Exercises/script.js
+++ AdvancedJS/Exercises/script.js
@@ -7693,3 +7693,22 @@ const array2 = [5,6,7,8]
 console.log(concatArrays(array1, array2))
 */
 
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+/*
+function findElement(arr, func){
+    let num = 0
+    for(let i = 0; i < arr.length; i++){
+        num = arr[i]
+        if(func(arr[i])){
+            return num
+        }
+
+    }
+
+    return undefined
+}
+
+console.log(findElement([1,2,3,4,5], num => num % 2 === 0))
+// 2
+*/
+
