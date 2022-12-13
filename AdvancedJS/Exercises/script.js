99ab71704a4e9fc8752280e4f827da0c499dd8ef
Phil Nelson <nelsonpa@miamioh.edu>
2022-12-14 08:41:20 +0900

    Loop through an array to return each item and wrap it in an <li> tag with a class text-warning using string template literals.

--- AdvancedJS/Exercises/script.js
+++ AdvancedJS/Exercises/script.js
@@ -8812,3 +8812,22 @@ function printName(currentPerson){
 printName(person)
 */
 
+const result = {
+        success: ["max-length", "no-amd", "prefer-arrow-functions"],
+        failure: ["no-var", "var-on-top", "linebreak"],
+        skipped: ["no-extra-semi", "no-dup-keys"]
+    }
+
+function makeList(arr) {
+        const failureItems = []
+        for(let i = 0; i < arr.length; i++){
+            failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+        }
+        return failureItems
+    }
+    
+const failuresList = makeList(result.failure)
+    console.log(failuresList)
+    // [ '<li class="text-warning">no-var</li>',
+    // '<li class="text-warning">var-on-top</li>',
+    // '<li class="text-warning">linebreak</li>' ]
