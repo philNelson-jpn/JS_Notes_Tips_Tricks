2bc9b7221ed1d3af7755e38796a92ed852ff6881
Phil Nelson <nelsonpa@miamioh.edu>
2023-01-21 16:04:16 +0900

    regex select 0 or more characters that appear in a row, look up a profile from an object nested in an array based on the name and prop of the object, change an impure function to a pure function

--- AdvancedJS/Exercises/script.js
+++ AdvancedJS/Exercises/script.js
@@ -8,6 +8,7 @@
 //     }
 // }
 
+
 /*
 Remove duplicates from an array:
 const array = [1, 2, 3, 4, 4, 3, 2, 5]
@@ -9663,5 +9664,76 @@ function randomRange(myMin, myMax){
 }
 
 console.log(randomRange(2, 6))
+*/
+
+// 1-21-2023
+// How do we select 0 or more characters like all of the a's in Aaaaaaaaargh?
+
+// let string = "Aaaaaaaargh"
+// let argRegex = /a*/i
+// let result = string.match(argRegex)
+// console.log(result)
+// // 'Aaaaaaaa'
+/*
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ]
+
+  function lookupProfile(name, prop){
+    for(let i = 0; i < contacts.length; i++){
+        const contact = contacts[i]
+        if(name === contact.firstName || name === contact.lastName){
+            if(contact.hasOwnProperty(prop)){
+                return contact[prop]
+            }else{
+                return "No such property"
+            }
+        }
+    }
+    return "Profile does not exist"
+  }
+
+  console.log(lookupProfile("Vos", "likes"))
+
+*/
+/*
+const person = {
+    name: "Phil",
+    friends: [
+        "Simon",
+        "Caleb"
+    ]
+}
+
+function addFriend(obj, friend){
+    return {...obj, friends: [...obj.friends, friend]}
+}
+
+const person2 = addFriend(person, "Jimmy")
+console.log(person2.friends)
+// ['Simon', 'Caleb', 'Jimmy']
 */
 
