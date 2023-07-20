f859f97aedaab83fde56e6cab9deeeab130475ab
Phil Nelson <nelsonpa@miamioh.edu>
2023-07-20 13:12:30 +0900

    Split a string and use regex to return an array of words that contains no spaces or special characters

--- AdvancedJS/AdvancedExercisesPt2/script.js
+++ AdvancedJS/AdvancedExercisesPt2/script.js
@@ -1976,3 +1976,31 @@ function logName(currentPerson) {
 
 logName(currentPerson)
 */
+
+// 7-20-2023
+// Split a string and use regex to return an array of words that contains no spaces or special characters.
+/*
+const string = "Hello goodbye see you farewell !  ?"
+const splitString = string.split(/\W+/)
+const filteredString = splitString.filter(word => word !== '')
+console.log(filteredString)
+// ['Hello', 'goodbye', 'see', 'you', 'farewell']
+*/
+
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'.
+/*
+function findElement(arr, func){
+	let num = 0
+	for(let i = 0; i < arr.length; i++){
+		num = arr[i]
+		if(func(num)){
+			return num
+		}
+	}
+	return undefined
+}
+
+
+console.log(findElement([1,2,3,4], num => num % 2 === 0))
+//2
+*/
