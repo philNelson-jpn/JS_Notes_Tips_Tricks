4508af59bf175095587f276667739cecac308680
Phil Nelson <nelsonpa@miamioh.edu>
2023-07-28 11:36:23 +0900

    Loop through an array of objects, use a nested if statement to check if one property matches one argument and if a property exists (second argument). Return no such property if it does not exist and return object does not exist if neither match

--- AdvancedJS/AdvancedExercisesPt2/script.js
+++ AdvancedJS/AdvancedExercisesPt2/script.js
@@ -2078,3 +2078,52 @@ function countOnline(userObj) {
 console.log(countOnline(users))
 // 2
 */
+
+// Loop through an array of objects, use a nested if statement to check if one property matches one argument and if a property exists (second argument). Return no such property if it does not exist and return object does not exist if neither match.
+/*
+const contacts = [
+	{
+		firstName: 'Akira',
+		lastName: 'Laine',
+		number: '0543236543',
+		likes: ['Pizza', 'Coding', 'Brownie Points'],
+	},
+	{
+		firstName: 'Harry',
+		lastName: 'Potter',
+		number: '0994372684',
+		likes: ['Hogwarts', 'Magic', 'Hagrid'],
+	},
+	{
+		firstName: 'Sherlock',
+		lastName: 'Holmes',
+		number: '0487345643',
+		likes: ['Intriguing Cases', 'Violin'],
+	},
+	{
+		firstName: 'Kristian',
+		lastName: 'Vos',
+		number: 'unknown',
+		likes: ['JavaScript', 'Gaming', 'Foxes'],
+	},
+]
+
+function lookUpProfile(name, prop){
+	for(let i = 0; i <= contacts.length; i++){
+		const contactName = contacts[i].firstName
+		const contactProp = contacts[i][prop]
+		const hasProp = contacts[i].hasOwnProperty(prop)
+		if(name === contactName){
+			if(hasProp){
+				return contactProp
+			}else {
+				return 'No such prop'
+			}
+		}
+	}
+	return "There ain't no profile, dummy"
+}
+
+console.log(lookUpProfile('Akira', 'likes'))
+// ['Pizza', 'Coding', 'Brownie Points']
+*/
